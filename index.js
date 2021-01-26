51d91ebd08428a7263f265b49648c09ea3d63124
HuangLiPang <leo89214749@hotmail.com>
2021-01-27 06:52:59 +0800

    rm redundant code

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const morgan = require("morgan");
 const createError = require("http-errors");
 const HTTPStatuses = require("statuses");
 const { initializer } = require("./db/initializer");
-require("dotenv").config();
 
 require("dotenv").config();
 initializer();
